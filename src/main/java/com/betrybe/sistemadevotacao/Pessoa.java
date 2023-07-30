797889b309b18fc85b3f65cb6102a73dbb652962
Jaron Morren <jaronmorren@gmail.com>
2023-07-29 21:07:40 -0300

    feat:implemented PessoaCandidata class

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,13 +1,35 @@
 package com.betrybe.sistemadevotacao;
 
+/**
+ * Abstract class representing persons.
+ */
 public abstract class Pessoa {
 
   protected String nome;
 
+  /**
+   * Constructor for creating a person.
+   *
+   * @param nome The name of the person.
+   */
+  public Pessoa(String nome) {
+    this.nome = nome;
+  }
+
+  /**
+   * Get the name of the person.
+   *
+   * @return The name of the person.
+   */
   public String getNome() {
     return nome;
   }
 
+  /**
+   * Set the name of the person.
+   *
+   * @param nome The name of the person to set.
+   */
   public void setNome(String nome) {
     this.nome = nome;
   }
--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,65 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+/**
+ * Class representing candidates.
+ */
+public class PessoaCandidata extends Pessoa {
 
+  private int numero;
+  private int votos;
+
+  /**
+   * Constructor for creating a candidate.
+   *
+   * @param nome   The name of the candidate.
+   * @param numero The candidate's number.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    super(nome);
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  /**
+   * Get the candidate's number.
+   *
+   * @return The candidate's number.
+   */
+  public int getNumero() {
+    return numero;
+  }
+
+  /**
+   * Get the number of votes received by the candidate.
+   *
+   * @return The number of votes received.
+   */
+  public int getVotos() {
+    return votos;
+  }
+
+  /**
+   * Set the candidate's number.
+   *
+   * @param numero The candidate's number to set.
+   */
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  /**
+   * Set the number of votes received by the candidate.
+   *
+   * @param votos The number of votes received to set.
+   */
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  /**
+   * Increment the number of votes received by one.
+   */
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
