f9c2fa7f519dc45950ed8565cc6b224099061d9f
Jaron Morren <jaronmorren@gmail.com>
2023-07-29 22:45:50 -0300

    feat:Implemented GerenciamentoVotacao class

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,5 +1,66 @@
 package com.betrybe.sistemadevotacao;
 
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+/**
+ * A class representing vote management. This class implements the GerenciamentoVotacaoInterface for
+ * managing the voting process.
+ */
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+
+  private ArrayList<PessoaCandidata> pessoasCandidatas;
+  private ArrayList<PessoaEleitora> pessoasEleitoras;
+  private ArrayList<String> cpfsComputados;
+
+  /**
+   * Constructor for creating a new instance of GerenciamentoVotacao. Initializes the lists for
+   * candidates, voters, and processed CPFs.
+   */
+  public GerenciamentoVotacao() {
+    pessoasCandidatas = new ArrayList<>();
+    pessoasEleitoras = new ArrayList<>();
+    cpfsComputados = new ArrayList<>();
+  }
+
+  /**
+   * Registers a new candidate for the election.
+   *
+   * @param nome   The name of the candidate.
+   * @param numero The candidate's identification number.
+   */
+  @Override
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    // Implementation of candidate registration.
+  }
+
+  /**
+   * Registers a new voter for the election.
+   *
+   * @param nome The name of the voter.
+   * @param cpf  The voter's unique identification number (CPF).
+   */
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+    // Implementation of voter registration.
+  }
+
+  /**
+   * Casts a vote for a specific candidate.
+   *
+   * @param cpfPessoaEleitora     The CPF of the voter casting the vote.
+   * @param numeroPessoaCandidata The identification number of the candidate receiving the vote.
+   */
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+    // Implementation of the voting process.
+  }
+
+  /**
+   * Displays the election results. This method will show the outcome of the voting process.
+   */
+  @Override
+  public void mostrarResultado() {
+    // Implementation to display the election results.
+  }
 }
+
