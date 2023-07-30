c9aa5e94dfb8a6721debd27ab322c60a16db8f8a
Jaron Morren <jaronmorren@gmail.com>
2023-07-29 21:53:26 -0300

    feat:Implemented PessoaEleitora Class

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,28 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+/**
+ * Class representing voters.
+ */
+public class PessoaEleitora extends Pessoa {
 
+  private String cpf;
+
+  /**
+   * Constructor for creating a voter.
+   *
+   * @param nome The name of the voter.
+   * @param cpf  The cpf of the voter.
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    super(nome);
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
